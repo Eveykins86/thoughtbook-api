0b0a1048ef2f23cc6ccb27064823705828ed93f8
Evelyn Jimenez <evey.plt@gmail.com>
2023-11-05 18:56:24 -0600

    added index.js code to root

---
+++ index.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const apiRoutes = require('./api');
+const app = express();
+const db = require('./connection')
+
+const port = process.env.PORT || 3001
+
+
+
+
+const main = async () => {
+    
+    db.once('connected', () => {
+        console.log('Database Connected');
+
+        app.use(express.json());
+        app.use(express.urlencoded({ extended: true }));
+        app.use(bodyParser.json())
+        app.use(apiRoutes)
+
+        app.listen(port, () => {
+            console.log('server is running on port ' + port)
+        });
+    })
+}
+
+
+main().catch(err => console.log(err))
